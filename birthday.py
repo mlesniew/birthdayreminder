6325e68d4a0fe83e6e2497fe2143755f7fb75f62
Michał Leśniewski <mlesniew@gmail.com>
2020-10-16 23:26:53 +0200

    Drop summary feature

--- birthday.py
+++ birthday.py
@@ -5,7 +5,6 @@ Notify about birthdays and anniversaries.
 
 Usage:
     bdnotify.py [options] list [--] [<days>...]
-    bdnotify.py [options] summary [--] [<year> <month>]
 
 Options:
     -f FILE --file=FILE     specify birthday file [default: ~/.birthday]
@@ -21,8 +20,6 @@ import docopt
 
 DATE_YMD = re.compile('([0-9]{1,4})-([0-9]{1,2})-([0-9]{1,2})')
 DATE_MD = re.compile('([0-9]{1,2})-([0-9]{1,2})')
-
-TODAY = datetime.date.today()
 
 
 class Event:
@@ -60,15 +57,11 @@ class Event:
     def days_remaining(self):
         return (self.next_date - self.today).days
 
-    def get_description_with_years(self, year=None):
-        if self.year:
-            return f'{self.description} ({year - self.year})'
-        else:
-            return self.description
-
     @property
     def reminder_text(self):
-        ret = self.get_description_with_years(self.next_date.year)
+        ret = self.description
+        if self.year:
+            ret += f' ({self.today.year - self.year})'
         days = self.days_remaining
         if days == 0:
             ret += 'today'
@@ -111,31 +104,13 @@ def main():
 
     events = list(parse(filename))
 
-    if args['summary']:
-        if args['<year>'] and args['<month>']:
-            when = datetime.date(int(args['<year>']), int(args['<month>']), 1)
-        else:
-            when = datetime.date.today()
+    advance_days = [int(a) for a in args['<days>']]
+    events = [event for event in events
+              if not advance_days or event.days_remaining in advance_days]
+    events.sort(key=lambda e: e.days_remaining)
 
-        events = [event for event in events
-                  if event.month == when.month and (not event.year or event.year <= when.year)]
-
-        events.sort(key=lambda e: e.day)
-
-        if events:
-            for e in events:
-                print(f'{when.year:04}-{when.month:02}-{e.day:02}: {e.get_description_with_years(when.year)}')
-        else:
-            print(f'No events in {when.year:04}-{when.month:02}')
-
-    else:
-        advance_days = [int(a) for a in args['<days>']]
-        events = [event for event in events
-                  if not advance_days or events.days_remaining in advance_days]
-        events.sort(key=lambda e: e.days_remaining)
-
-        for event in events:
-            print(event.reminder_text)
+    for event in events:
+        print(event.reminder_text)
 
 
 if __name__ == '__main__':
